be4c977f6d582c373705af72948cae20a4f318fe
hank583746309 <583746309@qq.com>
2015-10-25 11:08:44 +0800

    修正用户查询SQL 问题 添加留言默认处理

--- DDN/src/com/hoo/company/ddn/rpc/impl/ContactsMessageRpcImpl.java
+++ DDN/src/com/hoo/company/ddn/rpc/impl/ContactsMessageRpcImpl.java
@@ -6,6 +6,8 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.hoo.company.ddn.mudle.base.entity.BaseUser;
+import com.hoo.company.ddn.mudle.base.service.IBaseUserService;
 import com.hoo.company.ddn.mudle.contacts.entity.ContactsMessage;
 import com.hoo.company.ddn.mudle.contacts.service.IContactsMessageService;
 import com.hoo.company.ddn.rpc.IContactsMessageRpc;
@@ -16,7 +18,21 @@ public class ContactsMessageRpcImpl implements IContactsMessageRpc {
 	@Resource
 	IContactsMessageService messageService;
 	
+	@Resource
+	IBaseUserService userService;
+	
 	public boolean add(ContactsMessage message) throws SecurityException, NoSuchFieldException {
+		String toUserId = message.getToUserId();
+		//TODO 兼容处理
+		if(toUserId == null || "admin".equals(toUserId)){
+			BaseUser user = new BaseUser();
+			user.setUserName("admin");
+			user = userService.queryT(user);
+			if(null == user){
+				throw new NullPointerException("当前系统缺少admin账号,系统必须包含admin账号.");
+			}
+			message.setToUserId(user.getId());
+		}
 		return messageService.add(message) != null;
 	}
 
